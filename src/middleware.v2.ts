5e67daead9f9d02edb738af4de78154bcf1fc7cc
Luyzao <101422632+Luyzao@users.noreply.github.com>
2025-03-30 11:24:28 -0300

    configuration_conection_backend

---
+++ src/middleware.v2.ts
@@ -0,0 +1,51 @@
+// import cors from "cors";
+import { NextApiRequest, NextApiResponse } from "next";
+import nextCors from "nextjs-cors";
+// import config from "./configurations";
+// import { validadeToken } from "./services/middleware";
+
+export async function middlewareV2(
+  request: NextApiRequest,
+  response: NextApiResponse
+) {
+  response.setHeader('Cache-Control', 'no-store, max-age=0');
+  response.setHeader('Pragma', 'no-cache');
+  response.setHeader('Expires', '0');
+  response.setHeader("Access-Control-Allow-Origin", "*");
+  response.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, PATCH, OPTIONS");
+
+  /*
+  if (config.ENV == "PROD" || config.ENV == "STAG") {
+    response.setHeader( "Access-Control-Allow-Headers", "Content-Type, Authorization");
+
+    const authHeader = request.headers.authorization;
+    if (!authHeader || !authHeader.startsWith("Bearer ")) {
+      console.log('!authHeader || !authHeader.startsWith("Bearer ")');
+      return response.status(401).json({ message: "Token não autorizado." });
+    }
+
+    const token = authHeader.split(" ")[1];
+    console.log(token);
+    const isValid = validadeToken(token);
+    console.log(isValid);
+    if (!isValid) {
+      console.log('!isValid');
+      return response.status(401).json({ message: "Token não autorizado." });
+    }
+
+    response.setHeader("Authorization", `Bearer ${config.AUTH_TOKEN}`);
+  } else {
+    response.setHeader("Access-Control-Allow-Headers", "Content-Type");
+  }
+  */
+
+  await nextCors(request, response, {
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
+    origin: "*",
+    optionsSuccessStatus: 200,
+  });
+
+  if (request.method === "OPTIONS") {
+    return response.status(200).end();
+  }
+}
